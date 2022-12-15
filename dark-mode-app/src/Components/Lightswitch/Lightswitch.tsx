3342559c46029fe275639c7a4c7f9e84dc879660
GabrielFdeOliveira <gbl.diesel@hotmail.com>
2022-12-15 12:25:35 +0000

    icons library implementation attempt

--- dark-mode-app/src/Components/Lightswitch/Lightswitch.tsx
+++ dark-mode-app/src/Components/Lightswitch/Lightswitch.tsx
@@ -1,9 +1,12 @@
 import React, {useContext} from 'react';
 import {DarkModeContext} from '../DarkModeContext/DarkModeContext';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import Brightness2OutlinedIcon from '@mui/icons-material/Brightness2Outlined';
 
 import Light from './lightswitch-on.png'
 import LightOff from './lightswitch-off.png'
 
+let myIcon: React.ReactElement<SvgIconProps> = <WbSunnyIcon/>
 
 function Lightswitch() {
     const {darkMode, toggleDarkMode} = useContext(DarkModeContext);
@@ -13,10 +16,13 @@ function Lightswitch() {
     return (
         <div className="Lightswitch">
             <img src={darkMode ? 
-            Light : LightOff} alt="Lightswitch on" onClick={handleClick}/>          
+            Light : LightOff} alt="Lightswitch on" onClick={handleClick}/>  
+                
+            <div>
+                <img src={String (myIcon)} alt="Sun's icon"/>  
+
+            </div>    
         </div>
     )
 }
-
-
 export default Lightswitch
