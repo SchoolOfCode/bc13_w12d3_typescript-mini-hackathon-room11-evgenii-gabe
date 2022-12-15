069945ad860688adeaaf62d27c36f48413c6fee3
itsevgenii <smazanovich@icloud.com>
2022-12-15 13:33:21 +0000

    toggleButton works!

--- dark-mode-app/src/Components/Lightswitch/Lightswitch.tsx
+++ dark-mode-app/src/Components/Lightswitch/Lightswitch.tsx
@@ -2,11 +2,13 @@ import React, {useContext} from 'react';
 import {DarkModeContext} from '../DarkModeContext/DarkModeContext';
 import WbSunnyIcon from '@mui/icons-material/WbSunny';
 import Brightness2OutlinedIcon from '@mui/icons-material/Brightness2Outlined';
+import './Lightswitch.css'
+
 
 import Light from './lightswitch-on.png'
 import LightOff from './lightswitch-off.png'
 
-let myIcon: React.ReactElement<SvgIconProps> = <WbSunnyIcon/>
+
 
 function Lightswitch() {
     const {darkMode, toggleDarkMode} = useContext(DarkModeContext);
@@ -18,9 +20,11 @@ function Lightswitch() {
             <img src={darkMode ? 
             Light : LightOff} alt="Lightswitch on" onClick={handleClick}/>  
                 
-            <div>
-                <img src={String (myIcon)} alt="Sun's icon"/>  
-
+            <div className={darkMode ? "dark-mode-active" : "dark-mode"}>
+                <button className='btn-dark-mode'>
+                <WbSunnyIcon className="dmIconS" style={{display: darkMode ? "none" : "block"}}/>
+                <Brightness2OutlinedIcon className="dmIconM"  style={{display: darkMode ? "block" : "none"}}/>
+                </button>
             </div>    
         </div>
     )
